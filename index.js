64cc4e1ed0993099459388782cc1bb2d3e656de4
Nachimuthu N <nachi2muthu13@gmail.com>
2021-01-14 20:58:12 +0530

    removed upload to s3 part:

--- index.js
+++ index.js
@@ -39,37 +39,6 @@ const getZones = async (route53, zoneName) => {
 	return new Promise(resolve => setTimeout(resolve, ms));
   };
 
-  const uploadToS3 = async function( { systemFilePath, bucketPath, domainName } ) {
-	  const s3 = new AWS.S3({apiVersion: '2006-03-01'})
-	  
-	  try {
-		var file;
-		var count =  0;
-		while(count < 5)
-		{
-			try{
-				file = fs.readFileSync(systemFilePath)
-				break
-			} catch (e) {
-				await sleep(5000)
-				count += 1
-			}
-
-		}
-		const params = {
-			Bucket: process.env.AWS_S3_BUCKET_NAME,
-			Key: bucketPath,
-			Body: file
-		}
-		const fileUploaded = await s3.upload(params).promise();
-		console.log("Successfully Uploaded the certificate to the Bucket")
-        return true
-		} catch(err) {
-			console.log(err)
-			return false
-		}
-  }
-
 module.exports.create = function(config) {
 	const route53 = new AWS.Route53({apiVersion: "2013-04-01"});
 	const packageRoot = config.packageRoot
@@ -295,18 +264,7 @@ module.exports.create = function(config) {
 				})
 				.promise();
 			}
-
-			if(uploaded[ch.hostname] != undefined && !uploaded[ch.hostname] && config.staging == false) {
-				var systemFilePath = `${packageRoot}/${configDir}/live/${ch.dnsZone}/fullchain.pem`;
-				var bucketPath = `${config.bucketDirectoryPath}/${ch.hostname}/fullchain.pem`
-				const fullchainUpload = await uploadToS3({  systemFilePath, bucketPath})
-
-				systemFilePath = `${packageRoot}/${configDir}/live/${ch.dnsZone}/privkey.pem`;
-				bucketPath = `${config.bucketDirectoryPath}/${ch.hostname}/privkey.pem`
-				const privKeyUpload = await uploadToS3({ systemFilePath, bucketPath})
-				if(privKeyUpload && fullchainUpload) delete uploaded[ch.dnsZone]
-			}
-	
+				
 			return true;
 			} catch (e) {
 				console.log("Encountered an error deleting the record:", e);
